c605bb390a2828b77c393e195207796e6964cff0
ericsonmoreira <moreira.ericson@gmail.com>
2022-05-01 15:08:49 -0300

    ✨ criando o thema inicial

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Olá Mundo</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.palette.background,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ theme/index.tsx
@@ -0,0 +1,22 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  borderRadius: '4px',
+  palette: {
+    background: '#B5E7FF',
+    common: {
+      black: '#000',
+      white: '#FFF',
+    },
+    primary: {
+      main: '#366DDD',
+      contrastText: '#FFF',
+    },
+    secondary: {
+      main: '#00BFA6',
+      contrastText: '#000',
+    },
+  },
+};
+
+export default theme;
